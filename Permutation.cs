8cd6c8d8cdbce3319236f26045a0d7bf09fbff27
wasif789 <wsffarid1@gmail.com>
2021-10-04 07:57:46 +0530

    string permutations

---
+++ Permutation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmProblem
+{
+    class Permutation
+    {
+        public static List<string> recursiveList = new List<string>();
+
+        //Permutation using Recursion
+        public static void RecursivePermutation(String str, int l, int r)
+        {
+            if (l == r)
+            {
+                Console.WriteLine(str);
+                recursiveList.Add(str);
+            }
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    RecursivePermutation(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+        //Swap two array index
+        public static String swap(String a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace AlgorithmProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to algoritm program");
+            Console.WriteLine("Welcome To Algorithm Programs!");
+            Console.WriteLine("Enter 1-to Calculate Permutation of a string");
+
+            //Creating generics Object for generic Class
+
+
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    Console.WriteLine("Enter a string to calculate Permutation");
+                    string str = Console.ReadLine();
+                    int n = str.Length;
+                    Console.WriteLine("Permutation through Recursion Method");
+                    Permutation.RecursivePermutation(str, 0, n - 1);
+                    break;
+            }
         }
     }
 }
